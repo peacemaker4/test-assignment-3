46164dbf71ecf240af5ded2a11faaf114cd259ba
peacemaker4 <78819130+peacemaker4@users.noreply.github.com>
2025-08-19 21:13:56 +0500

    feat: display categories list from json

---
+++ menu.js
@@ -0,0 +1,66 @@
+const sceneRoot = document.getElementById("scene-root");
+const menuPath = document.getElementById("menu-path");
+
+let currentList = [];
+
+//Category, Subcategory
+function renderList(categories) {
+    sceneRoot.innerHTML = "";
+
+    categories.forEach((cat, i) => {
+
+        const box = document.createElement("a-box");
+        box.setAttribute("position", `${(i - categories.length/2) * 2 + 1} 0 -3`);
+        box.setAttribute("width", "1.5");
+        box.setAttribute("height", "1.5");
+        box.setAttribute("depth", "0.2");
+        box.setAttribute("color", cat.color);
+
+        const text = document.createElement("a-text");
+        text.setAttribute("value", cat.name);
+        text.setAttribute("align", "center");
+        text.setAttribute("color", "#000000");
+        text.setAttribute("position", "0 0 0.1");
+        text.setAttribute("width", "4.5");
+        box.appendChild(text);
+
+        box.addEventListener("click", () => {
+        if (cat.options) {
+            currentList.push({ data: cat.options });
+            renderList(cat.options);
+        } else {
+            currentList.push({ data: [] });
+            renderItem(cat);
+        }
+        });
+
+        sceneRoot.appendChild(box);
+    })
+}
+
+//Item
+function renderItem(item) {
+    sceneRoot.innerHTML = "";
+
+    const entity = document.createElement("a-entity");
+    entity.setAttribute("geometry", "primitive: dodecahedron; height: 0.8;");
+    entity.setAttribute("position", "0 0 -3");
+    entity.setAttribute("color", item.color);
+
+    const text = document.createElement("a-text");
+    text.setAttribute("value", item.name);
+    text.setAttribute("align", "center");
+    text.setAttribute("color", "#000000");
+    text.setAttribute("position", "0 1.25 0");
+    entity.appendChild(text);
+
+    sceneRoot.appendChild(entity);
+}
+
+//Load json data
+fetch("https://raw.githubusercontent.com/peacemaker4/test-assignment-3/refs/heads/main/menu-options.json").then(response => response.json())
+  .then(jdata => {
+    currentList = [{data: jdata.menu_options }];
+    renderList(jdata.menu_options);
+  })
+  .catch(err => console.error(err));
